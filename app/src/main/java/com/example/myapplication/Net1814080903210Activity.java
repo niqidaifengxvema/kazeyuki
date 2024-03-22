a50346b70f18f6d19f9a328eee32b190ded4cd18
25028 <3164210793@qq.com>
2024-03-22 08:29:15 +0800

    commit

---
+++ app/src/main/java/com/example/myapplication/Net1814080903210Activity.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.activity.ComponentActivity;
+
+public class Net1814080903210Activity extends ComponentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 在布局中找到 ID 为 "helloTextView" 的 TextView
+        TextView helloTextView = findViewById(R.id.helloTextView);
+
+        // 将 TextView 的文本设置为 "Hello World!"
+        helloTextView.setText("Hello World!");
+    }
+}
